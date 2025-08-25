3d674aa1a47f586e643be5a513a40e4beeaa2674
Yuliya Kuliyeva <yuliyakuliyeva@MacBookPro.lan>
2025-08-26 00:01:51 +0200

    Add HW-3.txt

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -4,3 +4,12 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+
+let input=6;
+let output;
+
+if (0 < input && input < 10){
+    let n = String (input);
+    output = Number(n) + (Number(n+n))+ Number(n+n+n);
+console.log(output);
+}
