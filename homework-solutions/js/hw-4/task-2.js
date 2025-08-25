e8360a3c17919b6cf8651277131687c8615324a8
Yuliya Kuliyeva <yuliyakuliyeva@MacBookPro.lan>
2025-08-26 00:50:36 +0200

    Add HW-4

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -14,3 +14,25 @@
   5. Проверить задание со следующими значениями в переменной age: 10, 17, 18, 19, 59, 60, 61
 
   */
+
+let minAge = 18;
+let maxAge = 60;
+let age = 'k';
+
+const incorrectTypeMessage = 'Incorrect data type';
+const defaultMessage = 'Technical work';
+const number = 'number';
+
+if (typeof age !== number) {
+  console.log(incorrectTypeMessage);
+} else {
+  if (age < minAge) {
+    console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
+  } else if (age >= minAge && age < maxAge) {
+    console.log('Welcome  !');
+  } else if (age > maxAge) {
+    console.log('Keep calm and look Culture channel');
+  } else {
+    console.log(defaultMessage);
+  }
+}
