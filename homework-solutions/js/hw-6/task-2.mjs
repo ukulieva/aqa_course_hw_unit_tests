b70df4a39046f4b8df8a04ff01a2c30d3a43eb04
Yuliya Kuliyeva <yuliyakuliyeva@MacBookPro.lan>
2025-09-03 00:01:59 +0200

    Add HW-6: task 1 and 2

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -13,7 +13,42 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
-let resultUnique;
-let resultNull;
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+const allmyPizzas = [];
+const allmyPizzasLC = [];
+const resultUnique = [];
+const competitorPizzasLC = [];
+let resultNull = null;
 
+for (const pizzaT1 of myPizzasT1) {
+  let pizzaT1LC = pizzaT1.toLowerCase();
+  if (!allmyPizzasLC.includes(pizzaT1LC)) {
+    allmyPizzas.push(pizzaT1);
+    allmyPizzasLC.push(pizzaT1LC);
+  }
+}
+
+for (const pizzaT2 of myPizzasT2) {
+  let pizzaT2LC = pizzaT2.toLowerCase();
+  if (!allmyPizzasLC.includes(pizzaT2LC)) {
+    allmyPizzas.push(pizzaT2);
+    allmyPizzasLC.push(pizzaT2LC);
+  }
+}
+
+for (const pizza of competitorPizzas) {
+  competitorPizzasLC.push(pizza.toLowerCase());
+}
+
+for (let i = 0; i < allmyPizzasLC.length; i++) {
+  if (!competitorPizzasLC.includes(allmyPizzasLC[i])) {
+    resultUnique.push(allmyPizzas[i]);
+  }
+}
+
+if (resultUnique.length === 0) {
+  resultNull = null;
+}
 export { resultNull, resultUnique };
