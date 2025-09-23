98a4420558010453e37df87942f9638bb6256532
Yuliya Kuliyeva <yuliyakuliyeva@MacBookPro.lan>
2025-09-23 22:08:50 +0200

    Add HW-4: task3

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,25 @@
   преобразовываясь в number
 
 */
+let minAge = 18;
+let maxAge = 60;
+let age = ' 33hh';
+
+const incorrectTypeMessage = 'Incorrect data type';
+const defaultMessage = 'Technical work';
+
+let newAge = Number(age);
+
+if (isNaN(newAge)) {
+  console.log(incorrectTypeMessage);
+} else {
+  if (newAge < minAge) {
+    console.log("You don't have access cause your age is " + newAge + " It's less then " + minAge);
+  } else if (newAge >= minAge && newAge < maxAge) {
+    console.log('Welcome  !');
+  } else if (newAge > maxAge) {
+    console.log('Keep calm and look Culture channel');
+  } else {
+    console.log(defaultMessage);
+  }
+}
