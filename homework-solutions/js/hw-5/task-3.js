52710576820390d0b4b3288d3ab1d692d51fdf94
Yuliya Kuliyeva <yuliyakuliyeva@MacBookPro.lan>
2025-09-23 23:01:44 +0200

    Add HW-5: task3

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -4,7 +4,20 @@
  * Ожидаемый результат для `hello`: "hello contains 2 vowels and 3 consonants".
  */
 const word = 'hello';
+const vowels = 'aeiou';
+const consonants = 'bcdfghjklmnpqrstvwxyz';
 
-let vowelsAndConsonantsResult = '';
+const wordArr = [...word];
+let vowelsCount = 0;
+let consonantsCount = 0;
 
+for (let letter of wordArr) {
+  if (vowels.includes(letter)) {
+    vowelsCount++;
+  } else if (consonants.includes(letter)) {
+    consonantsCount++;
+  }
+}
+
+let vowelsAndConsonantsResult = word + ' contains ' + vowelsCount + ' vowels and ' + consonantsCount + ' consonants';
 export { vowelsAndConsonantsResult };
