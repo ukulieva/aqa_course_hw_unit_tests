f1edeb49634d1b9660169792b87a4a4b6db26c9b
Yuliya Kuliyeva <yuliyakuliyeva@Yuliyas-MacBook-Pro-4.local>
2025-09-29 16:42:00 +0200

    Add HW-15: task 2

--- homework-solutions/ts/hw-15/task2.ts
+++ homework-solutions/ts/hw-15/task2.ts
@@ -3,3 +3,13 @@
 Если значение не найдено, функция должна возвращать undefined.
 Используйте keyof для типизации ключей объекта
 */
+
+function getKeyByValue<T extends object, U>(object: T, value: U): keyof T | undefined {
+  const findKey = (Object.keys(object) as (keyof T)[]).find((k) => object[k] === value);
+  return findKey;
+}
+
+const user = { id: 1, name: 'Юля', active: true };
+console.log(getKeyByValue(user, 'Юля'));
+console.log(getKeyByValue(user, 1));
+console.log(getKeyByValue(user, false));
